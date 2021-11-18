1564ce8cd4f6da1e1737f1ff143c6d4a0bc5174c
Michael Heimes <michael@cairndigitalmedia.com>
2021-11-18 14:50:43 -0500

    banner slider

--- template-parts/demo-or-ebook/demo-ebook-cta.php
+++ template-parts/demo-or-ebook/demo-ebook-cta.php
@@ -11,6 +11,9 @@
 <p class="request-demo">
   <a href="<?php bloginfo('url'); ?>/products/request-a-demo/" class="btn btn--alt request-demo">Request a demo</a>
 </p>
+
+<?php if(!is_front_page()):?>
 <p class="request-ebook">
   <a href="#request-ebook" class="btn request-ebook">Download free e-book</a>
 </p>
+<?php endif;?>
